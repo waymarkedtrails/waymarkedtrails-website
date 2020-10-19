343c8eb0be367dedaf4f6dc1a3a5c5101f1ca773
Sarah Hoffmann <lonvia@denofr.de>
2020-10-19 21:10:48 +0200

    clean up footer toolbar
    Move SVGs in separate files and introduce a common type ButtonFooter.

--- src/MapFooter.svelte
+++ src/MapFooter.svelte
@@ -1,8 +1,11 @@
 <script>
-  import 'bootstrap/dist/css/bootstrap.css';
-  import ButtonSidePanel from './ui/ButtonSidePanel.svelte';
-  import SearchForm from './ui/SearchForm.svelte';
-  import ButtonGeolocate from './ui/ButtonGeolocate.svelte';
+    import 'bootstrap/dist/css/bootstrap.css';
+    import { show_page } from './app_state.js';
+    import { enable_geolocation } from './map/LayerGeolocate.svelte';
+    import SearchForm from './ui/SearchForm.svelte';
+    import ButtonFooter from './ui/ButtonFooter.svelte';
+    import GeoPin from './svg/GeoPin.svelte';
+    import Gear from './svg/Gear.svelte';
 </script>
 
 <style>
@@ -18,16 +21,12 @@
 
 <div class="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
     <SearchForm />
-    <div class="btn-group" role="group" aria-label="Basic example">
-        <ButtonSidePanel page="settings">
-            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-gear-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 0 0-5.86 2.929 2.929 0 0 0 0 5.858z"/>
-            </svg>
-        </ButtonSidePanel>
-        <ButtonGeolocate />
-        <button type="button" class="btn btn-secondary">Right</button>
+    <div class="btn-group" role="group">
+        <ButtonFooter on:click={() => show_page('settings')}><Gear /></ButtonFooter>
+        <ButtonFooter on:click={enable_geolocation}><GeoPin /></ButtonFooter>
+        <ButtonFooter>Right</ButtonFooter>
     </div>
     <div class="btn-group" role="group">
-        <ButtonSidePanel page="routelist">Routes</ButtonSidePanel>
+        <ButtonFooter on:click={() => show_page('routelist')}>Routes</ButtonFooter>
     </div>
 </div>
