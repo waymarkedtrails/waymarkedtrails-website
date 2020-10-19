775f89be6227c3bcd2d89384b9d9f2b5ce3761b1
Sarah Hoffmann <lonvia@denofr.de>
2020-10-19 21:41:19 +0200

    move footer code into main app

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,26 @@
 <script>
     import '../css/global.css';
+    import 'bootstrap/dist/css/bootstrap.css';
 
     import { onDestroy } from 'svelte';
     import WMTConfig from 'theme';
-    import { basemap_id, map_opacity_base, map_opacity_shade, map_opacity_route, page_state } from './app_state.js';
+    import { basemap_id, map_opacity_base, map_opacity_shade, map_opacity_route, page_state, show_page } from './app_state.js';
     import { get_page_state, get_basemap, get_opacity } from './util/saved_state.js';
     import Map from './Map.svelte';
     import MapXYZLayer from './MapXYZLayer.svelte';
     import MapGeolocateLayer from './map/LayerGeolocate.svelte';
     import Headline from './Headline.svelte';
-    import MapFooter from './MapFooter.svelte';
+    import PageFooter from './PageFooter.svelte';
     import PanelRouteList from './PanelRouteList.svelte';
     import PanelRouteDetails from './PanelRouteDetails.svelte';
     import PanelSettings from './PanelSettings.svelte';
     import PanelSearch from './PanelSearch.svelte';
     import UpdateInfo from './UpdateInfo.svelte';
+    import { enable_geolocation } from './map/LayerGeolocate.svelte';
+    import SearchForm from './ui/SearchForm.svelte';
+    import ButtonFooter from './ui/ButtonFooter.svelte';
+    import GeoPin from './svg/GeoPin.svelte';
+    import Gear from './svg/Gear.svelte';
 
     let copyright;
     let sidepanel = '';
@@ -40,11 +46,23 @@
   <MapXYZLayer name="routelayer" url={WMTConfig.TILE_URL} opacity={$map_opacity_route}/>
   <MapGeolocateLayer />
 </Map>
+
 <Headline>
   <span slot="subleft"><UpdateInfo /></span>
   <span slot="subright"><span id="map-attribution"></span></span>
 </Headline>
-<MapFooter />
+
+<PageFooter>
+    <SearchForm />
+    <div class="btn-group" role="group">
+        <ButtonFooter on:click={() => show_page('settings')}><Gear /></ButtonFooter>
+        <ButtonFooter on:click={enable_geolocation}><GeoPin /></ButtonFooter>
+        <ButtonFooter>Right</ButtonFooter>
+    </div>
+    <div class="btn-group" role="group">
+        <ButtonFooter on:click={() => show_page('routelist')}>Routes</ButtonFooter>
+    </div>
+</PageFooter>
 
 {#if sidepanel === 'settings'}<PanelSettings/>{/if}
 {#if sidepanel === 'routelist'}<PanelRouteList/>{/if}
---
+++ src/PageFooter.svelte
@@ -0,0 +1,14 @@
+<style>
+  .btn-toolbar {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    margin: 0;
+    padding: 4px;
+    background-color: var(--theme-background-color);
+  }
+</style>
+
+<div class="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups">
+    <slot></slot>
+</div>
