68e175468760e40c462e8158579889dfcd01eddb
Sarah Hoffmann <lonvia@denofr.de>
2021-02-05 22:01:29 +0100

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -44,13 +44,15 @@ export default {
 	},
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css: css => {
-				css.write('bundle.css');
-			}
+            compilerOptions: {
+                // enable run-time checks when not in production
+                dev: !production,
+                // we'll extract any component CSS out into
+                // a separate file - better for performance
+                css: css => {
+                    css.write('bundle.css');
+                }
+            }
 		}),
 
 		// If you have external dependencies installed from
