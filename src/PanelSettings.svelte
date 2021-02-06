80f49589b9fb9406569657464024ffd5b292d69a
Sarah Hoffmann <lonvia@denofr.de>
2021-02-06 23:25:54 +0100

    implement language switch

--- src/PanelSettings.svelte
+++ src/PanelSettings.svelte
@@ -1,13 +1,44 @@
 <script>
-    import { locale, locales, _ } from 'svelte-i18n';
+    import { onMount } from 'svelte';
+    import { _ } from 'svelte-i18n';
+    import { languages, set_stored_locale, get_stored_locale } from './i18n.js';
     import { basemap_id, map_opacity_base, map_opacity_route, map_opacity_shade } from './app_state.js';
     import WMTConfig from '../theme.js';
     import SidePanel from './ui/SidePanel.svelte';
     import OpacitySlider from './ui/OpacitySlider.svelte';
     import DropdownList from './ui/DropdownList.svelte';
+
+    let current_language = null;
+    let current_lang_title = null;
+
+    function set_language(ev) {
+        set_stored_locale(ev.target.getAttribute('data-lang'));
+        current_lang_title = ev.target.getAttribute('data-title');
+    }
+
+    onMount(() => {
+        current_language = get_stored_locale();
+
+        if (current_language !== null) {
+            languages.forEach((item, idx) => {
+                if (item[0] == current_language) {
+                    current_lang_title = item[1];
+                }
+            });
+        }
+    });
 </script>
 
 <SidePanel title={$_('settings.title')}>
+    <h4>{$_('settings.language')}</h4>
+
+    <DropdownList title={current_lang_title || $_('Use browser setting')}>
+        <button class="dropdown-item" type="button" on:click={set_language}>{$_('Use browser setting')}</button>
+        {#each languages as lang}
+            <button class="dropdown-item" type="button" data-lang={lang[0]} data-title={lang[1]} on:click={set_language}>{lang[1]}</button>
+        {/each}
+    </DropdownList>
+
     <h4>{$_('settings.base_map')}</h4>
 
     <DropdownList title={WMTConfig.BASEMAPS[$basemap_id].name}>
@@ -23,12 +54,4 @@
       <OpacitySlider name="route" title={$_('settings.route_layer')} bind:value={$map_opacity_route} />
       <OpacitySlider name="shade" title={$_('settings.hillshading_layer')} bind:value={$map_opacity_shade} />
     </div>
-
-    <h4>{$_('settings.language')}</h4>
-
-    <DropdownList title="TODO">
-        {#each $locales as loc}
-            <button class="dropdown-item" type="button" on:click="{() => locale.set(loc)}">{loc}</button>
-        {/each}
-    </DropdownList>
 </SidePanel>
--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,30 @@
-import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
+import { register, init, getLocaleFromNavigator, locale } from 'svelte-i18n';
+
+export const languages = [['en', 'English'],
+                          ['de', 'Deutsch']
+                         ];
 
 register('en', () => import('./i18n/en.json'));
 register('de', () => import('./i18n/de.json'));
 
+let storedLocale = localStorage.getItem('lang');
+
 init({
   fallbackLocale: 'en',
-  initialLocale: getLocaleFromNavigator(),
+  initialLocale: storedLocale === null ? getLocaleFromNavigator() : storedLocale,
 });
+
+export function set_stored_locale(lang) {
+    storedLocale = lang;
+    if (lang === null) {
+        localStorage.removeItem('lang');
+        locale.set(getLocaleFromNavigator());
+    } else {
+        localStorage.setItem('lang', lang);
+        locale.set(lang);
+    }
+}
+
+export function get_stored_locale() {
+    return storedLocale;
+}
