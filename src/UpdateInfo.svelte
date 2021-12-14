d48fe0afe0a913861447d5c4967dc7b9f60dcc1c
Sarah Hoffmann <lonvia@denofr.de>
2021-12-14 22:22:28 +0100

    use svelte-i18n for date localisation

--- src/UpdateInfo.svelte
+++ src/UpdateInfo.svelte
@@ -1,21 +1,21 @@
 <script>
-    import { _ } from 'svelte-i18n';
+    import { _, date, time } from 'svelte-i18n';
     import { onMount, onDestroy } from 'svelte';
     import { json_loader } from './util/load_json.js';
 
     let db_update = '';
+    let error_msg = '';
 
     const loader = json_loader(function(data) {
         if (data.server_status == 'OK') {
-            const update = new Date(Date.parse(data.last_update));
-            db_update = update.toLocaleString(navigator.languages);
+            db_update = new Date(Date.parse(data.last_update));
         } else {
-            db_update = "API unavailable";
+            error_msg = "API unavailable";
         }
-    }, function(error) { db_update = error; });
+    }, function(error) { error_msg = error; });
 
     onMount(function() { loader.load('/status'); });
     onDestroy(function() { loader.abort(); });
 </script>
 
-{$_('headline.last_update')}: {db_update}
+{$_('headline.last_update')}: {#if error_msg}{error_msg}{:else}{$date(db_update)} {$time(db_update)}{/if}
