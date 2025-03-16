efbcfe025c35b9b6f86bda6b1c857fad2cf92930
Sarah Hoffmann <lonvia@denofr.de>
2025-03-16 22:20:54 +0100

    add missing linear state file

---
+++ src/ui/LinearState.svelte
@@ -0,0 +1,34 @@
+<script>
+import { _ } from 'svelte-i18n';
+
+let { state = 'no' } = $props();
+
+</script>
+
+<style>
+    span {
+         padding-left: 0.5em
+    }
+
+    .linear-style-yes {
+         color: #3C3
+    }
+
+    .linear-style-sorted {
+         color: #992
+    }
+
+    .linear-style-no {
+         color: #C33
+    }
+</style>
+
+<span class="linear-style-{state}" title={$_('details.linear.' + state)}>
+{#if state == 'no'}
+✘
+{:else if state == 'sorted'}
+↭
+{:else}
+✔
+{/if}
+</span>
