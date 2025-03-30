eb5c7ac49c41477289401b1bdcb952f57f0ce55e
Sarah Hoffmann <lonvia@denofr.de>
2025-03-30 11:36:01 +0200

    respect explicitly choosen languages

--- src/util/load_json.js
+++ src/util/load_json.js
@@ -1,10 +1,17 @@
 import { API_URL } from '../config.js';
+import { get_stored_locale } from '../i18n.js';
 
 export function json_load(url, params, signal) {
     let urlobj = new URL(url.startsWith('http') ? url : API_URL + url);
     urlobj.search = new URLSearchParams(params).toString();
 
-    return fetch(urlobj, {signal})
+    const options = {signal: signal};
+    const current_language = get_stored_locale();
+    if (current_language !== null) {
+        options.headers = {'Accept-Language': current_language};
+    }
+
+    return fetch(urlobj, options)
         .catch((e) => {throw new Error('error.load_error'); })
         .then((res) => {
             if (res.ok) {
